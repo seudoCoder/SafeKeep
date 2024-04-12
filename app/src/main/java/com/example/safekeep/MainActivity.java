55b86d46b0d0b7079f8e3f3601c8a23a4a2c7935
KirthiLogin <kirthikasubbu7@gmail.com>
2024-04-12 18:01:43 +0530

    COMMIT 2

--- app/src/main/java/com/example/safekeep/MainActivity.java
+++ app/src/main/java/com/example/safekeep/MainActivity.java
@@ -1,22 +1,64 @@
 package com.example.safekeep;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.Query;
 
 public class MainActivity extends AppCompatActivity {
 
-    FloatingActionButton addNotebtn;
+    FloatingActionButton addNoteBtn;
+    RecyclerView recyclerView;
+    ImageButton menuBtn;
+    NoteAdapter noteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         addNotebtn=findViewById(R.id.add_note_btn);
-         addNotebtn.setOnClickListener((v)->startActivity(new Intent(MainActivity.this,NoteDetailActivity.class)));
+         addNoteBtn=findViewById(R.id.add_note_btn);
+         recyclerView=findViewById(R.id.recyler_view);
+         menuBtn=findViewById(R.id.menu_btn);
+         addNoteBtn.setOnClickListener((v)->startActivity(new Intent(MainActivity.this,NoteDetailActivity.class)));
+         menuBtn.setOnClickListener((v)->showMenu());
+         setUpRecyclerView();
+    }
 
+
+
+    void showMenu() {
+    }
+
+    private void setUpRecyclerView() {
+        Query query=Utility.getCollectionReferenceForNotes().orderBy("timestamp",Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions<Note> options=new FirestoreRecyclerOptions.Builder<Note>().setQuery(query,Note.class).build();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        noteAdapter=new NoteAdapter(options,this);
+        recyclerView.setAdapter(noteAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        noteAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        noteAdapter.stopListening();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        noteAdapter.notifyDataSetChanged();
     }
 }
